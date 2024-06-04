090f8565a6acf7bb68be37a41d431cd2f3d361e3
ArhiCoden <goxaqud@gmail.com>
2024-06-04 19:16:01 +0300

    Fix bug e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :MyImg="MyImg" />
+</template>
+
+<script>
+import Vue from 'vue'
+import MyImg from '@MyImg/core'
+import { Dashboard } from '@MyImg/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    MyImg: () => new MyImg(),
+  },
+}
+</script>
+
+<style src="@MyImg/core/dist/style.css"></style>
+<style src="@MyImg/dashboard/dist/style.css"></style>
